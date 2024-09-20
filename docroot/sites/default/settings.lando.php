e72db64ecfb9d0513a5e28fd6420d6544f2cbf41
Stephen Cox <stephen@agile.coop>
2024-09-20 16:28:48 +0100

    Tweaks to Drupal config

---
+++ docroot/sites/default/settings.lando.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Lando database credentials.
+ */
+$databases['default']['default'] = array (
+  'database' => 'database',
+  'username' => 'database',
+  'password' => 'database',
+  'host' => 'database',
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+);
