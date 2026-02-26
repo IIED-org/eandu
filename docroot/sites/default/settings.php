b23f9612c98b56292e8b09cd76d7a51a64d8d88a
cbrody <chris@cbrody.com>
2026-02-26 14:14:29 +0700

    Add ddev for local test

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -527,3 +527,9 @@ if (file_exists(DRUPAL_ROOT . '/' . conf_path() . '/settings.local.php')) {
 elseif (getenv('LANDO_INFO') !== FALSE && file_exists(DRUPAL_ROOT . '/' . conf_path() . '/settings.lando.php')) {
   include DRUPAL_ROOT . '/' . conf_path() . '/settings.lando.php';
 }
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
